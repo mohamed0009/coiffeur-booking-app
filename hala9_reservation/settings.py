5741255c01b322cce2c79fefdb8c268b5d37f1f5
Your Name <your.email@example.com>
2025-06-24 17:20:19 +0100

    Prepare project for Render deployment

--- hala9_reservation/settings.py
+++ hala9_reservation/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e&@f@&#28*+jxkx)(7x=3oayc&t!jt_3gz#zik*g#)*c$%@&fo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not os.environ.get('RENDER')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
